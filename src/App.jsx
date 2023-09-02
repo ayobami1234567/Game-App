5662302d48d1eabb5ad99345bf3cdc86bc4c8eda
cccspl <110902142+cccspl@users.noreply.github.com>
2023-09-02 17:36:24 +0100

    h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useEffect, useState } from 'react'
 import Home from './Pages/Home'
 import Header from './Components/Header'
 import { ThemeContext } from './Context/ThemeContext'
-
 const App = () => {
-  const [theme, setTheme] = useState("dark")
+  const [theme, setTheme] = useState("light")
 
   useEffect(() => {
     setTheme(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark')
   })
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className={`${theme} ${theme == 'dark' ? 'bg-[#121212]' : null} h-[100vh]`}>
+      <div className={`${theme} ${theme == 'dark' ? 'bg-[#121212] text-white' : null}`}>
         <Header />
         <Home />
       </div>
--- src/Components/GenreList.jsx
+++ src/Components/GenreList.jsx
@@ -3,24 +3,29 @@ import GlobalApi from "../Services/GlobalApi"
 
 const GenreList = () => {
 
-  const [genreList, setGenreList] = useState([])
-
+  const [genreList, setGenreList] = useState([]); // Initialize as an empty array
   useEffect(() => {
     getGenreList();
-  }, [])
+  }, []); // Add an empty dependency array to run this effect only once
+
   const getGenreList = () => {
     GlobalApi.getGenreList.then((resp) => {
-      console.log(resp.data.resuits);
-      setGenreList(resp.data.resuits);
-    })
-  }
-  return (
-    <>
-      <div>
-        <h2 className="text-[30px] font-bold">Genre</h2>
-      </div>
-    </>
-  )
-}
+      console.log(resp.data.results); // Use 'results' instead of 'resuits'
+      setGenreList(resp.data.results);
+    });
+  };
 
-export default GenreList
+  return (
+    <div>
+      <h2 className="text-[30px] font-bold dark:text-white p-8">Genre</h2>
+      {genreList.map((item, index) => (
+        <div key={index} className="flex  gap-2 items-center cursor-pointer hover:bg-gray-300 group  p-2 rounded-lg hover:dark:bg-gray-600 transition-all duration-300"> {/* Add a unique 'key' prop for each mapped item */}
+          <img src={item.background_image} className="w-[40px] h-[40px] object-cover rounded-lg" alt="" />
+          <h3 className=" text-[18px] group-hover:scale-105">{item.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GenreList;
